8bd142a1561443985fa3c30d530ff19ce3d4bd17
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-11 21:22:32 +0500

    Подключает handlebars

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require(`express`);
 const path = require(`path`);
+const exphbs = require(`express-handlebars`);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({
+  defaultLayout: `main`,
+  extname: `hbs`
+});
+
+app.engine(`hbs`, hbs.engine); // регистрируем в Express, что вообще есть такой движок
+app.set(`view engine`, `hbs`); // начинаем его использовать
+app.set(`views`, `views`); // где лежать все шаблоны
 
 app.get(`/`, (req, res) => {
   res.sendFile(path.join(__dirname, `views`, `index.html`));
