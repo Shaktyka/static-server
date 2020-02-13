6fbd6b85518b3596f473352190cefbdb0ef047af
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-13 17:12:58 +0500

    Добавляет .editorconfig

--- models/course.js
+++ models/course.js
@@ -3,35 +3,35 @@ const fs = require(`fs`);
 const path = require(`path`);
 
 class Course {
-    constructor(title, price, imageUrl) {
-        this.title = title;
-        this.price = price;
-        this.image = imageUrl;
-        this.id = uuid();
-    }
+  constructor(title, price, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.image = imageUrl;
+    this.id = uuid();
+  }
 
-    async save() {
-        const courses = await Course.getAll();
-        console.log(courses);
-    }
+  async save() {
+    const courses = await Course.getAll();
+    console.log(courses);
+  }
 
-    static getAll() {
+  static getAll() {
 
-        return new Promise((res, rej) => {
-            fs.readFile(
-                path.join(__dirname, `../data/courses.json`),
-                `utf-8`,
-                (err, content) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                      console.log(content);
-                        // resolve(JSON.parse(content));
-                    }
-                }
-            )
-        });
-    }
+    return new Promise((res, rej) => {
+      fs.readFile(
+        path.join(__dirname, `../data/courses.json`),
+        `utf-8`,
+        (err, content) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(content);
+            // resolve(JSON.parse(content));
+          }
+        }
+      )
+    });
+  }
 }
 
 module.exports = Course;
